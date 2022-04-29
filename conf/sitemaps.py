1a0da9332d82b9780cf42e462a63c8e5971186ac
Jason Ade <92988726+Jasontechnologies@users.noreply.github.com>
2022-04-29 12:41:33 +0100

    Add files via upload

---
+++ conf/sitemaps.py
@@ -0,0 +1,19 @@
+from django.contrib.sitemaps import Sitemap
+from django.shortcuts import  reverse
+
+
+
+from posts.models import  *
+ 
+
+class CategorySitemap(Sitemap):
+    def items(self):
+        return Category.objects.all()
+        
+        
+class PostSitemap(Sitemap):
+    def items(self):
+        return Post.objects.filter(status=Post.ACTIVE)
+        
+    def lastmod(self,obj):
+        return obj.created_at
